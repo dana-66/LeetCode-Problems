3fc8e5b581d8f536e5b6ac978539feb1a60ae1ad
dana-66 <danatarik66@gmail.com>
2024-08-05 00:01:47 +0300

    28. Find the Index of the First Occurrence in a String

---
+++ LeetCode/Programming Skills/Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,15 @@
+// 28. Find the Index of the First Occurrence in a String
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if(haystack.includes(needle)){
+        return haystack.indexOf(needle);
+    }else{
+        return -1;
+    }
+};
